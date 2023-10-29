d819a875e068ab25b6621de88ec91ddcc7017d6b
Kirill <ibotlold@gmail.com>
2023-10-29 23:32:05 +0600

    feat: lang syntax

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,12 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int reverted = 0;
+        while (number > 0) {
+            reverted = reverted * 10 + number % 10;
+            number = number / 10;
+        }
+        System.out.println(reverted);
     }
 
 }
