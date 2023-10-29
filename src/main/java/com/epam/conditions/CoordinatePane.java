529701f372904df858de7f4601f36fcd3c4969e8
Kirill <ibotlold@gmail.com>
2023-10-30 00:25:54 +0600

    feat: conditions

--- src/main/java/com/epam/conditions/CoordinatePane.java
+++ src/main/java/com/epam/conditions/CoordinatePane.java
@@ -3,6 +3,23 @@ package com.epam.conditions;
 public class CoordinatePane {
 
     public void printQuadrant(int x, int y) {
+        if (x * y == 0) {
+            System.out.println("zero");
+            return;
+        }
+        if (x * y > 0) {
+            if (x > 0) {
+                System.out.println("first");
+            } else {
+                System.out.println("third");
+            }
+        } else {
+            if (y > 0) {
+                System.out.println("second");
+            } else {
+                System.out.println("fourth");
+            }
+        }
     }
 
 }
